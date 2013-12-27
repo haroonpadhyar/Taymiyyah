cd305b39ce1c8577fa2a9d6398f6bc3e958bbc1b
Haroon Anwar Padhyar <haroon.padhyar@gmail.com>
2013-12-27 20:37:02 +0400

    Input validation on searchServlet

--- taymiyyah-web-simple/src/main/java/com/maktashaf/taymiyyah/controller/SearchServlet.java
+++ taymiyyah-web-simple/src/main/java/com/maktashaf/taymiyyah/controller/SearchServlet.java
@@ -3,8 +3,10 @@ package com.maktashaf.taymiyyah.controller;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,6 +30,7 @@ import org.apache.log4j.Logger;
 public class SearchServlet extends HttpServlet{
   private static Logger logger = Logger.getLogger(SearchServlet.class);
   private int PAGE_SIZE = 1;
+  private Map<Integer, Integer> ayahCountMap = new HashMap<Integer, Integer>(114);
 
   QuranSearchService quranSearchService = new QuranSearchSearchServiceImpl();
 
@@ -72,6 +75,121 @@ public class SearchServlet extends HttpServlet{
   public void init() throws ServletException {
     String pageSize = getServletContext().getInitParameter("pageSize");
     PAGE_SIZE = Integer.valueOf(pageSize).intValue();
+    ayahCountMap.put(1,7);
+    ayahCountMap.put(2,286);
+    ayahCountMap.put(3,200);
+    ayahCountMap.put(4,176);
+    ayahCountMap.put(5,120);
+    ayahCountMap.put(6,165);
+    ayahCountMap.put(7,206);
+    ayahCountMap.put(8,75);
+    ayahCountMap.put(9,129);
+    ayahCountMap.put(10,109);
+    ayahCountMap.put(11,123);
+    ayahCountMap.put(12,111);
+    ayahCountMap.put(13,43);
+    ayahCountMap.put(14,52);
+    ayahCountMap.put(15,99);
+    ayahCountMap.put(16,128);
+    ayahCountMap.put(17,111);
+    ayahCountMap.put(18,110);
+    ayahCountMap.put(19,98);
+    ayahCountMap.put(20,135);
+    ayahCountMap.put(21,112);
+    ayahCountMap.put(22,78);
+    ayahCountMap.put(23,118);
+    ayahCountMap.put(24,64);
+    ayahCountMap.put(25,77);
+    ayahCountMap.put(26,227);
+    ayahCountMap.put(27,93);
+    ayahCountMap.put(28,88);
+    ayahCountMap.put(29,69);
+    ayahCountMap.put(30,60);
+    ayahCountMap.put(31,34);
+    ayahCountMap.put(32,30);
+    ayahCountMap.put(33,73);
+    ayahCountMap.put(34,54);
+    ayahCountMap.put(35,45);
+    ayahCountMap.put(36,83);
+    ayahCountMap.put(37,182);
+    ayahCountMap.put(38,88);
+    ayahCountMap.put(39,75);
+    ayahCountMap.put(40,85);
+    ayahCountMap.put(41,54);
+    ayahCountMap.put(42,53);
+    ayahCountMap.put(43,89);
+    ayahCountMap.put(44,59);
+    ayahCountMap.put(45,37);
+    ayahCountMap.put(46,35);
+    ayahCountMap.put(47,38);
+    ayahCountMap.put(48,29);
+    ayahCountMap.put(49,18);
+    ayahCountMap.put(50,45);
+    ayahCountMap.put(51,60);
+    ayahCountMap.put(52,49);
+    ayahCountMap.put(53,62);
+    ayahCountMap.put(54,55);
+    ayahCountMap.put(55,78);
+    ayahCountMap.put(56,96);
+    ayahCountMap.put(57,29);
+    ayahCountMap.put(58,22);
+    ayahCountMap.put(59,24);
+    ayahCountMap.put(60,13);
+    ayahCountMap.put(61,14);
+    ayahCountMap.put(62,11);
+    ayahCountMap.put(63,11);
+    ayahCountMap.put(64,18);
+    ayahCountMap.put(65,12);
+    ayahCountMap.put(66,12);
+    ayahCountMap.put(67,30);
+    ayahCountMap.put(68,52);
+    ayahCountMap.put(69,52);
+    ayahCountMap.put(70,44);
+    ayahCountMap.put(71,28);
+    ayahCountMap.put(72,28);
+    ayahCountMap.put(73,20);
+    ayahCountMap.put(74,56);
+    ayahCountMap.put(75,40);
+    ayahCountMap.put(76,31);
+    ayahCountMap.put(77,50);
+    ayahCountMap.put(78,40);
+    ayahCountMap.put(79,46);
+    ayahCountMap.put(80,42);
+    ayahCountMap.put(81,29);
+    ayahCountMap.put(82,19);
+    ayahCountMap.put(83,36);
+    ayahCountMap.put(84,25);
+    ayahCountMap.put(85,22);
+    ayahCountMap.put(86,17);
+    ayahCountMap.put(87,19);
+    ayahCountMap.put(88,26);
+    ayahCountMap.put(89,30);
+    ayahCountMap.put(90,20);
+    ayahCountMap.put(91,15);
+    ayahCountMap.put(92,21);
+    ayahCountMap.put(93,11);
+    ayahCountMap.put(94,8);
+    ayahCountMap.put(95,8);
+    ayahCountMap.put(96,19);
+    ayahCountMap.put(97,5);
+    ayahCountMap.put(98,8);
+    ayahCountMap.put(99,8);
+    ayahCountMap.put(100,11);
+    ayahCountMap.put(101,11);
+    ayahCountMap.put(102,8);
+    ayahCountMap.put(103,3);
+    ayahCountMap.put(104,9);
+    ayahCountMap.put(105,5);
+    ayahCountMap.put(106,4);
+    ayahCountMap.put(107,7);
+    ayahCountMap.put(108,3);
+    ayahCountMap.put(109,6);
+    ayahCountMap.put(110,3);
+    ayahCountMap.put(111,5);
+    ayahCountMap.put(112,4);
+    ayahCountMap.put(113,5);
+    ayahCountMap.put(114,6);
+
   }
 
   private SearchResult process(String term, int pageNo, LocaleEnum localeEnum, boolean original){
@@ -154,7 +272,9 @@ public class SearchServlet extends HttpServlet{
     if(localeEnum == null)
       localeEnum = LocaleEnum.Ar;
 
-    SearchResult searchResult = process(term, currentPage, localeEnum, original);
+    SearchResult searchResult = SearchResult.builder().withQuranList(new ArrayList<Quran>(1)).build();
+    if(null != term && term.length() > 0)
+        searchResult = process(term, currentPage, localeEnum, original);
     currentPage = Math.min(currentPage, searchResult.getTotalPages());
     long totalTime = System.currentTimeMillis() - startTime;
     ResultData resultData = new ResultData()
@@ -185,6 +305,7 @@ public class SearchServlet extends HttpServlet{
 
     int surahNo = 0;
     int ayahNo = 0;
+    Quran quran = null;
     try{
       surahNo = Integer.valueOf(surahId);
       ayahNo = Integer.valueOf(ayahId);
@@ -194,11 +315,15 @@ public class SearchServlet extends HttpServlet{
       ayahNo = 0;
     }
 
-    Quran quran = null;
-    if(radio.equals("idSrch"))
-      quran = quranSearchService.findByAyahId(surahNo, ayahNo, localeEnum);
-    else if(radio.equals("srSrch"))
-      quran = quranSearchService.findByAccumId(ayahNo, localeEnum);
+    if(radio.equals("idSrch")) {
+      if((surahNo > 0 && surahNo <= 114)
+          && (ayahNo > 0 && ayahNo <= ayahCountMap.get(surahNo)))
+        quran = quranSearchService.findByAyahId(surahNo, ayahNo, localeEnum);
+    }
+    else if(radio.equals("srSrch")){
+      if(ayahNo > 0 && ayahNo <= 6236)
+        quran = quranSearchService.findByAccumId(ayahNo, localeEnum);
+    }
 
     List<Quran> quranList = new ArrayList<Quran>();
     if(quran != null)
